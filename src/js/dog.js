a2518b0c0b4f835062f6cecb54c85eb13802d885
Ben Lee <leeohsunshine@gmail.com>
2016-03-21 18:49:56 -0400

    Added ending to dog.js to make tests pass with second Dog class.

--- src/js/dog.js
+++ src/js/dog.js
@@ -1,31 +1,31 @@
-export class Dog {
-	constructor(qualities = {}) {
-		this.status = "normal";
-		this.color = qualities.color;
-		if (qualities.hungry === undefined){this.hungry = true;
-		}else{this.hungry = qualities.hungry;}
-		this.owner = undefined;
-		console.log(this)
-  };
-};
-
-// var DEFAULT_STATUS = "normal";
-// var DEFAULT_COLOR = undefined;
-// var DEFAULT_HUNGRY = true;
-// var DEFAULT_OWNER = undefined;
-
 // export class Dog {
 // 	constructor(qualities = {}) {
-// 		this.qualities = Object.assign({
-// 			status: DEFAULT_STATUS,
-// 			color: DEFAULT_COLOR,
-// 			hungry: DEFAULT_HUNGRY,
-// 			owner: DEFAULT_OWNER
-// 		}, qualities);
-// 		console.log(this.qualities)
+// 		this.status = "normal";
+// 		this.color = qualities.color;
+// 		if (qualities.hungry === undefined){this.hungry = true;
+// 		}else{this.hungry = qualities.hungry;}
+// 		this.owner = undefined;
 //   };
 // };
 
-// I’m getting all of the correct Dogs created with the second class
-// as far as I can tell. 
-// But when their qualities are tested the answers are not passing.
+var DEFAULT_STATUS = "normal";
+var DEFAULT_COLOR = undefined;
+var DEFAULT_HUNGRY = true;
+var DEFAULT_OWNER = undefined;
+
+export class Dog {
+	constructor(qualities = {}) {
+		this.qualities = Object.assign({
+			status: DEFAULT_STATUS,
+			color: DEFAULT_COLOR,
+			hungry: DEFAULT_HUNGRY,
+			owner: DEFAULT_OWNER
+		}, qualities);
+
+		var {status, color, hungry, owner} = this.qualities;
+		this.status = status;
+		this.color = color;
+		this.hungry = hungry;
+		this.owner = owner;
+  };
+};
